9bf0de7eb6936cbc9f8241bc5f492df774f28bec
Itamar <ihassin@gmail.com>
2026-03-12 11:09:58 +0200

    ETL works using parameters, and is logging to the output log stream

--- crawler-create.sh
+++ crawler-create.sh
@@ -1,13 +1,17 @@
 #!/bin/zsh
 
 STAGING_BUCKET_NAME="com-in-context-data-load-staging"
+DATA_PATH=city-data
 
+echo "Running stack"
 aws cloudformation deploy --template-file glue-crawler.yaml --stack-name com-in-context-city-data-etl --capabilities CAPABILITY_NAMED_IAM \
 --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION"
 
-aws s3 sync ./data/ "s3://$STAGING_BUCKET_NAME/city-data" --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION"
+echo "Loading data and script"
+aws s3 sync ./data/ "s3://$STAGING_BUCKET_NAME/$DATA_PATH" --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION"
 aws s3 sync ./scripts/ "s3://$STAGING_BUCKET_NAME/scripts" --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION"
 
+echo "Starting crawler"
 aws glue start-crawler --name CityDataCrawler \
 --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION"
 
@@ -18,5 +22,13 @@ while true; do
   sleep 10
 done
 
+echo "Starting ETL"
 aws glue start-job-run --job-name city-data-etl-job \
 --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION"
+#--arguments "{
+#  \"--BUCKET_NAME\": \"s3://$STAGING_BUCKET_NAME\",
+#  \"--DATA_FILE_LOCATION\": \"$DATA_PATH\",
+#  \"--DATA_FILE_NAME\": \"CityData.csv\",
+#  \"--PROCESSED_DATA_LOCATION\": \"processed\"
+#  }"
+
--- scripts/city_etl.py
+++ scripts/city_etl.py
@@ -1,11 +1,32 @@
 import sys
+import logging
+
 from awsglue.utils import getResolvedOptions
+from awsglue.job import Job
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
-from awsglue.job import Job
 from pyspark.sql.functions import col, when
 
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+# args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(handler)
+
+logger.info("*** Transformation start")
+
+args = getResolvedOptions(
+    sys.argv,
+    [
+        "JOB_NAME",
+        "BUCKET_NAME",
+        "DATA_FILE_LOCATION",
+        "DATA_FILE_NAME",
+        "PROCESSED_DATA_LOCATION"
+    ]
+)
+logger.info(f"*** Args: {args}")
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -14,8 +35,13 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
+bucket_name = args["BUCKET_NAME"]
+data_file_location = args["DATA_FILE_LOCATION"]
+data_file_name = args["DATA_FILE_NAME"]
+processed_data_location = args["PROCESSED_DATA_LOCATION"]
+
 df = spark.read.option("header","true").csv(
-    "s3://com-in-context-data-load-staging/city-data/CityData.csv"
+    f"{bucket_name}/{data_file_location}/{data_file_name}"
 )
 
 df = df.withColumn(
@@ -23,9 +49,14 @@ df = df.withColumn(
     when((col("State").isNull()) | (col("State") == ""), "n/a")
     .otherwise(col("State"))
 )
+logger.info(f"*** Transformed Record count: {df.count()}")
+logger.info("*** Preview transformed data:")
+for row in df.take(5):
+    logger.info(row)
 
 df.write.mode("overwrite").parquet(
-    "s3://com-in-context-data-load-staging/processed/"
+    f"{bucket_name}/{processed_data_location}/"
 )
 
 job.commit()
+logger.info("*** Transformation complete")
