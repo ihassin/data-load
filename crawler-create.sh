b02f770c44df4c3bbbffadb7c348d08e34fe3b45
Itamar <ihassin@gmail.com>
2026-03-16 10:40:34 +0200

    Works; Needs some final testing

--- crawler-create.sh
+++ crawler-create.sh
@@ -3,7 +3,7 @@
 STAGING_BUCKET_NAME="com-in-context-data-load-staging"
 DATA_PATH=city-data
 
-echo "Running stack"
+echo "Submitting stack"
 aws cloudformation deploy --template-file glue-crawler.yaml --stack-name com-in-context-city-data-etl --capabilities CAPABILITY_NAMED_IAM \
 --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION"
 
@@ -24,30 +24,49 @@ done
 
 echo "Crawler finished"
 
+#---
+
 #--arguments "{
 #  \"--BUCKET_NAME\": \"s3://$STAGING_BUCKET_NAME\",
 #  \"--DATA_FILE_LOCATION\": \"$DATA_PATH\",
 #  \"--DATA_FILE_NAME\": \"CityData.csv\",
 #  \"--PROCESSED_DATA_LOCATION\": \"processed\"
 #  }"
-echo "Starting ETL Job"
-RUN_ID=$(aws glue start-job-run --job-name city-data-etl-job --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION" --output text)
+#echo "Starting ETL Job"
+#RUN_ID=$(aws glue start-job-run --job-name city-data-etl-job --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION" --output text)
+#
+#echo "Job Run ID: $RUN_ID"
+#
+#while true; do
+#  STATE=$(aws glue get-job-run \
+#    --job-name city-data-etl-job \
+#    --run-id "$RUN_ID" \
+#    --query 'JobRun.JobRunState' \
+#    --output text \
+#    --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION")
+#
+#  echo "State: $STATE"
+#
+#  if [[ "$STATE" == "SUCCEEDED" || "$STATE" == "FAILED" || "$STATE" == "STOPPED" ]]; then
+#    break
+#  fi
+#
+#  sleep 10
+#done
 
-echo "Job Run ID: $RUN_ID"
 
-while true; do
-  STATE=$(aws glue get-job-run \
-    --job-name city-data-etl-job \
-    --run-id "$RUN_ID" \
-    --query 'JobRun.JobRunState' \
-    --output text \
-    --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION")
+#echo "Initializing triggers"
+#aws glue start-trigger --name StartCityDataPipelineTrigger \
+#  --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION"
+#
+#aws glue start-trigger --name RunDataQualityTrigger \
+#  --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION"
 
-  echo "State: $STATE"
+echo "Workflow graph"
+aws glue get-workflow --name city-data-workflow --include-graph --output text \
+  --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION"
 
-  if [[ "$STATE" == "SUCCEEDED" || "$STATE" == "FAILED" || "$STATE" == "STOPPED" ]]; then
-    break
-  fi
-
-  sleep 10
-done
+echo "Starting workflow"
+RUN_ID=$(aws glue start-workflow-run --name city-data-workflow --query 'RunId' --output text --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION")
+aws glue get-workflow-run --name city-data-workflow --run-id $RUN_ID --output text \
+  --profile "$AWS_TW_PROFILE" --region "$AWS_PERSONAL_REGION"
